b0e38a139fb65315d65546785e9aee8520298cfa
taka <github@forestwinds.com>
2015-02-21 15:25:37 +0900

    pre-check the existence of MINE.jar

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject mine-clj "0.1.0-SNAPSHOT"
+(defproject mine-clj "0.1.1-SNAPSHOT"
   :description "mine-clj: a simple clojure wrapper for MINE.jar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :resource-paths ["./lib/MINE.jar"]
+  :injections [(when-not (.exists (clojure.java.io/file "./lib/MINE.jar"))
+                (throw (Exception. "\nMINE.jar is not found.\nDownload it from http://www.exploredata.net/ftp/MINE.jar\n")))]
   ;:jvm-opts ["-Xms256M", "-Xmx4096M"]
   :main mine-clj.core)
